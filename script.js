364e69d78c561a05c1afc6bbe426e4129815214b
MramRashidR <mramrashid98@gmail.com>
2022-10-04 17:01:08 +0300

    js three - click / changeRole

--- script.js
+++ script.js
@@ -1,12 +1,86 @@
-const board = document.querySelector(".board-container")
-const cell = document.querySelectorAll(".grid")
-const reset = document.querySelector(".button")
-const msg = document.querySelector(".msg-container")
+let board = document.querySelector(".board-container")
+let cells = document.querySelectorAll(".grid")
+let reset = document.querySelector(".button")
+let msg = document.querySelector(".msg-container")
 /* const firstPlayer = document.querySelector(".firstPlayer")
 const secondPlayer = document.querySelector(".secondPlayer") */
 
-    const secondPlayer = 1;
-    const firstPlayer = 2;
+let currentPlyr = "firstPlayer"
+
+// const secondPlayer = 1;
+let  winnerArrays = [
+    [0, 1, 2, 3],
+    [41, 40, 39, 38],
+    [7, 8, 9, 10],
+    [34, 33, 32, 31],
+    [14, 15, 16, 17],
+    [27, 26, 25, 24],
+    [21, 22, 23, 24],
+    [20, 19, 18, 17],
+    [28, 29, 30, 31],
+    [13, 12, 11, 10],
+    [35, 36, 37, 38],
+    [6, 5, 4, 3],
+    [0, 7, 14, 21],
+    [41, 34, 27, 20],
+    [1, 8, 15, 22],
+    [40, 33, 26, 19],
+    [2, 9, 16, 23],
+    [39, 32, 25, 18],
+    [3, 10, 17, 24],
+    [38, 31, 24, 17],
+    [4, 11, 18, 25],
+    [37, 30, 23, 16],
+    [5, 12, 19, 26],
+    [36, 29, 22, 15],
+    [6, 13, 20, 27],
+    [35, 28, 21, 14],
+    [0, 8, 16, 24],
+    [41, 33, 25, 17],
+    [7, 15, 23, 31],
+    [34, 26, 18, 10],
+    [14, 22, 30, 38],
+    [27, 19, 11, 3],
+    [35, 29, 23, 17],
+    [6, 12, 18, 24],
+    [28, 22, 16, 10],
+    [13, 19, 25, 31],
+    [21, 15, 9, 3],
+    [20, 26, 32, 38],
+    [36, 30, 24, 18],
+    [5, 11, 17, 23],
+    [37, 31, 25, 19],
+    [4, 10, 16, 22],
+    [2, 10, 18, 26],
+    [39, 31, 23, 15],
+    [1, 9, 17, 25],
+    [40, 32, 24, 16],
+    [9, 17, 25, 33],
+    [8, 16, 24, 32],
+    [11, 17, 23, 29],
+    [12, 18, 24, 30],
+    [1, 2, 3, 4],
+    [5, 4, 3, 2],
+    [8, 9, 10, 11],
+    [12, 11, 10, 9],
+    [15, 16, 17, 18],
+    [19, 18, 17, 16],
+    [22, 23, 24, 25],
+    [26, 25, 24, 23],
+    [29, 30, 31, 32],
+    [33, 32, 31, 30],
+    [36, 37, 38, 39],
+    [40, 39, 38, 37],
+    [7, 14, 21, 28],
+    [8, 15, 22, 29],
+    [9, 16, 23, 30],
+    [10, 17, 24, 31],
+    [11, 18, 25, 32],
+    [12, 19, 26, 33],
+    [13, 20, 27, 34],
+  ];
+    // const firstPlayer = 2;
+    // let box = 0;
   
   let userClickedElements = [
     "", "", "", "", "", "", "",
@@ -15,104 +89,34 @@ const secondPlayer = document.querySelector(".secondPlayer") */
     "", "", "", "", "", "", "",
     "", "", "", "", "", "", "",
     "", "", "", "", "", "", "", 
-    ]
+    ];
     
-    let currentPlyr = 1
-    let running = false ;
+    // let running = false ;
 
 
-  function startGame(cell) {
-    cell.forEach(cell=> cell.addEventListener("click" , addPiece));
+  function startGame() {
+    cells.forEach(cell=> cell.addEventListener("click", addPiece));
     reset.addEventListener("click" , resetGame);
     running = true
   }
 
 
   function addPiece (event) {
-
+    let clickedCell = event.target
+    clickedCell.classList.add(currentPlyr)
+    changeRole()
 // take event.target
 // add piece to that element
 // update userClickedElements with the correct index
-
-
   }
+
+
   function changeRole () {
-    currentPlyr = (currentPlyr == 1) ? 2 : 1;
+    currentPlyr = (currentPlyr == "firstPlayer") ? "secondPlayer" : "firstPlayer";
   }
 
     
   function CheckingWinning (fourConnected) {
-    let  winnerArrays = [
-        [0, 1, 2, 3],
-        [41, 40, 39, 38],
-        [7, 8, 9, 10],
-        [34, 33, 32, 31],
-        [14, 15, 16, 17],
-        [27, 26, 25, 24],
-        [21, 22, 23, 24],
-        [20, 19, 18, 17],
-        [28, 29, 30, 31],
-        [13, 12, 11, 10],
-        [35, 36, 37, 38],
-        [6, 5, 4, 3],
-        [0, 7, 14, 21],
-        [41, 34, 27, 20],
-        [1, 8, 15, 22],
-        [40, 33, 26, 19],
-        [2, 9, 16, 23],
-        [39, 32, 25, 18],
-        [3, 10, 17, 24],
-        [38, 31, 24, 17],
-        [4, 11, 18, 25],
-        [37, 30, 23, 16],
-        [5, 12, 19, 26],
-        [36, 29, 22, 15],
-        [6, 13, 20, 27],
-        [35, 28, 21, 14],
-        [0, 8, 16, 24],
-        [41, 33, 25, 17],
-        [7, 15, 23, 31],
-        [34, 26, 18, 10],
-        [14, 22, 30, 38],
-        [27, 19, 11, 3],
-        [35, 29, 23, 17],
-        [6, 12, 18, 24],
-        [28, 22, 16, 10],
-        [13, 19, 25, 31],
-        [21, 15, 9, 3],
-        [20, 26, 32, 38],
-        [36, 30, 24, 18],
-        [5, 11, 17, 23],
-        [37, 31, 25, 19],
-        [4, 10, 16, 22],
-        [2, 10, 18, 26],
-        [39, 31, 23, 15],
-        [1, 9, 17, 25],
-        [40, 32, 24, 16],
-        [9, 17, 25, 33],
-        [8, 16, 24, 32],
-        [11, 17, 23, 29],
-        [12, 18, 24, 30],
-        [1, 2, 3, 4],
-        [5, 4, 3, 2],
-        [8, 9, 10, 11],
-        [12, 11, 10, 9],
-        [15, 16, 17, 18],
-        [19, 18, 17, 16],
-        [22, 23, 24, 25],
-        [26, 25, 24, 23],
-        [29, 30, 31, 32],
-        [33, 32, 31, 30],
-        [36, 37, 38, 39],
-        [40, 39, 38, 37],
-        [7, 14, 21, 28],
-        [8, 15, 22, 29],
-        [9, 16, 23, 30],
-        [10, 17, 24, 31],
-        [11, 18, 25, 32],
-        [12, 19, 26, 33],
-        [13, 20, 27, 34],
-      ];
 
       for(let winnerArray of winnerArrays) {
         if ( 
@@ -138,17 +142,13 @@ const secondPlayer = document.querySelector(".secondPlayer") */
         "", "", "", "", "", "", "",
         "", "", "", "", "", "", "",
         "", "", "", "", "", "", "", 
+        console.log("kkkkkkk")
         ];
 
-        cell.forEach(cell => cell.textContent = "");
+        cells.forEach(cell => cell.textContent = "");
         running = true;
   }
 
-  function msg() {
-    
 
 
-  }
-
-
-
+startGame()
