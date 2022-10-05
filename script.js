e88d66dfa81c1c17bcd112a500a6508edc3765ad
MramRashidR <mramrashid98@gmail.com>
2022-10-05 21:13:32 +0300

    modifc

--- script.js
+++ script.js
@@ -5,21 +5,6 @@ let msg = document.querySelector(".msg-container")
 /* const firstPlayer = document.querySelector(".firstPlayer")
 const secondPlayer = document.querySelector(".secondPlayer") */
 let currentPlyr = "firstPlayer"
-
-
-let columns = [
-    [0 , 7 , 14 , 21 , 28 , 35] , 
-    [1 , 8 , 15 , 22 , 29 , 36] , 
-    [2 , 9 , 16 , 23 , 30 , 37] , 
-    [3 , 10, 17 , 24 , 31 , 38] , 
-    [4 , 11 , 18 , 25 , 32 , 39] ,
-    [5 , 12 , 19 , 26 , 33 , 40] , 
-    [6 , 13 , 20 , 27 , 34 , 41]
-
-];
-
-
-// const secondPlayer = 1;
 let  winnerArrays = [
     [0, 1, 2, 3],
     [41, 40, 39, 38],
@@ -91,8 +76,6 @@ let  winnerArrays = [
     [12, 19, 26, 33],
     [13, 20, 27, 34],
   ];
-    // const firstPlayer = 2;
-    // let box = 0;
   
   let userClickedElements = [
     "", "", "", "", "", "", "",
@@ -106,7 +89,7 @@ let  winnerArrays = [
     let running = false ;
 
 
-  function startGame() {
+function startGame() {
     reset.addEventListener('click', ResetGame)
     cells.forEach(cell=> cell.addEventListener("click", addPiece));
     running = true
@@ -114,7 +97,7 @@ let  winnerArrays = [
 
 
 function addPiece (event) {
-   
+    
     let clickedCell = event.target
     const conditionA = clickedCell.classList.length === 1
     const conditionB = clickedCell.id >=35 
@@ -125,7 +108,7 @@ function addPiece (event) {
             changeRole()
         } else {
             const conditionC = document.getElementById(parseInt(clickedCell.id) + 7).classList.length !== 1
-
+            
             if (conditionC) {
                 clickedCell.classList.add(currentPlyr)
                 changeRole()
@@ -135,116 +118,16 @@ function addPiece (event) {
 }
 
 
-  function changeRole () {
+function changeRole () {
     currentPlyr = (currentPlyr == "firstPlayer") ? "secondPlayer" : "firstPlayer";
-    
-    
 }
 
 
-// function startFromBottom () {
     
-    //     if 
-    
-    // }
-    
-    
-    function CheckingWinning (fourConnected) {
-
-        let  winnerArrays = [
-            [0, 1, 2, 3],
-            [41, 40, 39, 38],
-            [7, 8, 9, 10],
-            [34, 33, 32, 31],
-            [14, 15, 16, 17],
-            [27, 26, 25, 24],
-            [21, 22, 23, 24],
-            [20, 19, 18, 17],
-            [28, 29, 30, 31],
-            [13, 12, 11, 10],
-            [35, 36, 37, 38],
-            [6, 5, 4, 3],
-            [0, 7, 14, 21],
-            [41, 34, 27, 20],
-            [1, 8, 15, 22],
-            [40, 33, 26, 19],
-            [2, 9, 16, 23],
-            [39, 32, 25, 18],
-            [3, 10, 17, 24],
-            [38, 31, 24, 17],
-            [4, 11, 18, 25],
-            [37, 30, 23, 16],
-            [5, 12, 19, 26],
-            [36, 29, 22, 15],
-            [6, 13, 20, 27],
-            [35, 28, 21, 14],
-            [0, 8, 16, 24],
-            [41, 33, 25, 17],
-            [7, 15, 23, 31],
-            [34, 26, 18, 10],
-            [14, 22, 30, 38],
-            [27, 19, 11, 3],
-            [35, 29, 23, 17],
-            [6, 12, 18, 24],
-            [28, 22, 16, 10],
-            [13, 19, 25, 31],
-            [21, 15, 9, 3],
-            [20, 26, 32, 38],
-            [36, 30, 24, 18],
-            [5, 11, 17, 23],
-            [37, 31, 25, 19],
-            [4, 10, 16, 22],
-            [2, 10, 18, 26],
-            [39, 31, 23, 15],
-            [1, 9, 17, 25],
-            [40, 32, 24, 16],
-            [9, 17, 25, 33],
-            [8, 16, 24, 32],
-            [11, 17, 23, 29],
-            [12, 18, 24, 30],
-            [1, 2, 3, 4],
-            [5, 4, 3, 2],
-            [8, 9, 10, 11],
-            [12, 11, 10, 9],
-            [15, 16, 17, 18],
-            [19, 18, 17, 16],
-            [22, 23, 24, 25],
-            [26, 25, 24, 23],
-            [29, 30, 31, 32],
-            [33, 32, 31, 30],
-            [36, 37, 38, 39],
-            [40, 39, 38, 37],
-            [7, 14, 21, 28],
-            [8, 15, 22, 29],
-            [9, 16, 23, 30],
-            [10, 17, 24, 31],
-            [11, 18, 25, 32],
-            [12, 19, 26, 33],
-            [13, 20, 27, 34],
-          ];
-        
-        for(let winnerArray of winnerArrays) {
-            if ( 
-            fourConnected[winnerArray[0]].textContent == fourConnected[winnerArray[1]].textContent 
-            && fourConnected[winnerArray[1]].textContent == fourConnected[winnerArray[2]].textContent
-            && fourConnected[winnerArray[2]].textContent == fourConnected[winnerArray[3]].textContent
-            && fourConnected[winnerArray[3]].textContent == fourConnected[winnerArray[4]].textContent
-            ) {
-                return `${currentPlyr} Won!`; 
-            }
-        }
-        
-    }
-    
-    
-   function ResetGame () {
+function ResetGame () {
     cells.forEach(cell => {
         cell.classList.remove('firstPlayer')
         cell.classList.remove('secondPlayer')
     })
-
-
-   }
-
-
+}
 startGame();
